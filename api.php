3194b44d2333ab33155acf7219cefa44788bcd91
GP <santiago.gomezp@davinci.edu.ar>
2025-11-25 15:14:04 -0300

    manejo de errores de api

--- api.php
+++ api.php
@@ -9,6 +9,9 @@ $busqueda = isset($_GET['q']) ? $_GET['q'] : '';
 
 $categoria = isset($_GET['categoria']) ? $_GET['categoria'] : '';
 
+$busqueda = addslashes($busqueda);
+$categoria = addslashes($categoria);
+
 $sql = "SELECT * FROM albuns WHERE 1=1";
 
 if ($busqueda != '') {
@@ -18,9 +21,12 @@ if ($categoria != '' && $categoria != 'todas') {
     $sql .= " AND categoria = '$categoria'";
 }
 
+$sql .= " ORDER BY id DESC";
+
 try {
     $resultados = $db->consultar($sql);
     echo json_encode($resultados);
 } catch (Exception $e) {
+    http_response_code(500);
     echo json_encode(['error' => 'Error al consultar la base de datos']);
 }
